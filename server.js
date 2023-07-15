71aae7b393812bffe3e200b2307758fafa0d72ac
Sukrit <roysukrit78@gmail.com>
2023-07-15 19:24:56 +0530

    Added Views

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const experess = require('express');
 const app = experess();
 const port = 3003;
 
+//Setting Template Engine
+app.set("view engine","pug")
+app.set("views","views")
 const server  = app.listen(port,()=>console.log("App Started"));
 
 
 app.get('/',(req,res,next) => {
-    res.status(200).send("Ok")
+
+
+
+    const payload ={
+        pageTitle:"Home"
+    }
+
+
+    res.status(200).render("home",payload)
 
 })
