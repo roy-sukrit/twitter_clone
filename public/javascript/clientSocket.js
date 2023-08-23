bd1fa90be9cbe16bb03b0172243d4324bfc500c7
Sukrit <roysukrit78@gmail.com>
2023-08-23 18:28:45 +0530

    added socket url

--- public/javascript/clientSocket.js
+++ public/javascript/clientSocket.js
@@ -1,6 +1,6 @@
 var connected = false;
 
-var socket = io("http://localhost:3003")
+var socket = io("https://twitter-clone-ecde.onrender.com/")
 socket.emit("setup", userLoggedIn);
 
 socket.on("connected", () => connected = true);
